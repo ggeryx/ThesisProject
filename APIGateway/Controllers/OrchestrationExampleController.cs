aa3d43e931de0a6d9b60bbc27f37ee16695680c6
G Geryx <gorcsos.gergely@gmail.com>
2023-12-12 17:33:05 +0100

    Added swagger to the gateway, mapped a controller that will later be used

---
+++ APIGateway/Controllers/OrchestrationExampleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrchestrationExampleController : ControllerBase
+    {
+        private readonly ILogger<OrchestrationExampleController> logger;
+
+        public OrchestrationExampleController(ILogger<OrchestrationExampleController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ThisIsSparta")]
+        public async Task<IActionResult> ThisIsSparta()
+        {
+
+
+            return Ok();
+        }
+    }
+}
